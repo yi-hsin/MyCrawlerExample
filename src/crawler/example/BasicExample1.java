6bfd39a6bb2c0d8b71a42ef4dbaa121ce864e221
yi-hsin <enidwang99@gmail.com>
2016-12-11 12:25:03 +0800

    test3

---
+++ src/crawler/example/BasicExample1.java
@@ -0,0 +1,55 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class BasicExample1 {
+	// commit test
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "https://www.ptt.cc/man/Gossiping/D29E/D21D/D476/D491/M.1471759508.A.9CF.html";
+
+		Document doc=
+				CrawlerPack.start()
+				
+				// 參數設定
+
+
+			    .addCookie("over18","1")	// 設定cookie
+				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+				
+				// 選擇資料格式 (三選一)
+				//.getFromJson(uri)
+			    .getFromHtml(uri);
+			    //.getFromXml(uri)
+			    
+			    // 這兒開始是 Jsoup Document 物件操作
+			    //.select("main-content *")
+		;
+
+
+		for (Element elem:doc.select("#main-content  *")){
+			//System.out.println(elem.toString());
+			elem.remove();
+		}
+
+		System.out.println(doc.select("#main-content ").text());
+
+
+	}
+}
